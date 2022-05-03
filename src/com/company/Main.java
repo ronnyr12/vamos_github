f3aad1249d3c1c4bdb3591572d3b60d5d4e48b07
Ronny <ronny761@hityash.org>
2022-05-03 11:00:59 +0300

    Initial commit - 1st time uploading to github servers

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        elStringo(new String[]{"vamos!!", "quiero dormir", "si y no", ""});
+    }
+
+    /**
+     * lesson practice
+     * @param strings
+     * @return true - if array contain strings with more than 1 word or the word finshed with ! or a
+     */
+    public static boolean elStringo(String[] strings) {
+        //todo - complete method algo
+        return true;
+    }
+}
